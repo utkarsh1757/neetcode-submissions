daf09b1b18f7c499c9c612f07f6e4cbb5f7cf7e6
Utkarsh Rathore <utkarshrathore.workmail@gmail.com>
2026-07-21 18:12:24 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const map1 = new Map()
+        const map2 = new Map()
+        if (s.length == t.length) {
+            for (let i = 0; i < s.length; i++) {
+                map1.set(s[i],(map1.get(s[i]) ||0)+1)
+                map2.set(t[i],(map2.get(t[i]) ||0)+1)
+            }
+            for(let key of map1.keys()){
+                if(map1.get(key)!== map2.get(key)){
+                    return false
+                }
+            }
+            return true
+        }else{
+            return false
+        }
+    }
+    // if(s.length !== t.length){
+    //     return false
+    // }
+    // const map = new Map<String,number>();
+    // for(let i =0; i < s.length; i++){
+
+    // }
+}
